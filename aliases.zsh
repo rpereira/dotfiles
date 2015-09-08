17846c3d7b58c8e957d0d2df371c63ce18404dd3
ruiafonsopereira <ruiafonsopereira@gmail.com>
2015-09-08 10:10:24 +0100

    Adds aliases.

---
+++ aliases.zsh
@@ -0,0 +1,7 @@
+# General
+alias zshconfig="vim ~/.zshrc"
+alias myzshconfig="vim $ZSH_CUSTOM/rap.zsh"
+
+# Shiftit
+alias shiftit="bundle exec foreman start -f Procfile.dev"
+alias tail_log="tail -f log/development.log"
