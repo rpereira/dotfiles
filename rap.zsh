2c9a25703ae82deab6a47f3c32cb2170939c1cfd
Rui Afonso Pereira <rpereira@users.noreply.github.com>
2015-09-08 10:10:00 +0100

    Extracts aliases to a separate file.

--- rap.zsh
+++ rap.zsh
@@ -1,13 +1,11 @@
+# Load additional files.
+source $ZSH_CUSTOM/aliases.zsh
+
 # Activate the theme for all future shell sessions.
 eval `dircolors $ZSH_CUSTOM/dircolors_solarized/dircolors.ansi-light`
 
 # Use the 16 colors terminal option to get VIM to look like GVIM with solarized colors.
 set t_Co=16
 
-alias zshconfig="atom ~/.zshrc"
-
 # Shiftit
 export SHIFTIT="/home/rap/repos/shiftit"
-
-alias shiftit="cd $SHIFTIT && bundle exec foreman start -f Procfile.dev"
-alias log="tail -f log/development.log"
