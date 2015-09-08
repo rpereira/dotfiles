de11a5da6fe876b3ed816c18b1ff3ebdcd21728f
ruiafonsopereira <ruiafonsopereira@gmail.com>
2015-09-08 08:55:25 +0100

    Fixes conflicting background colors in VIM.

--- rap.zsh
+++ rap.zsh
@@ -1,4 +1,8 @@
+# Activate the theme for all future shell sessions.
 eval `dircolors $ZSH_CUSTOM/dircolors_solarized/dircolors.ansi-light`
+
+# Use the 16 colors terminal option to get VIM to look like GVIM with solarized colors.
+set t_Co=16
 
 alias zshconfig="atom ~/.zshrc"
 
