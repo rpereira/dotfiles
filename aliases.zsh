6946b70856d54cbb263a3f1cd07ce3b7ef2f8b69
ruiafonsopereira <ruiafonsopereira@gmail.com>
2015-09-08 10:18:35 +0100

    Adds vagrant aliases.

--- aliases.zsh
+++ aliases.zsh
@@ -5,3 +5,9 @@ alias myzshconfig="vim $ZSH_CUSTOM/rap.zsh"
 # Shiftit
 alias shiftit="bundle exec foreman start -f Procfile.dev"
 alias tail_log="tail -f log/development.log"
+
+# Vagrant
+alias v="vagrant"
+alias vs="vagrant status"
+alias vh="vagrant halt"
+alias vu="vagrant up"
