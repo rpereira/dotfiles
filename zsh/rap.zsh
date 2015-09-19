fca7ec5bf3a4e05a33b069887c16b8f6bd7fbc10
Rui Pereira <ruiafonsopereira@gmail.com>
2015-09-19 01:16:16 +0100

    Add awesome cd movements

--- zsh/rap.zsh
+++ zsh/rap.zsh
@@ -1,8 +1,8 @@
 # Activate the theme for all future shell sessions.
 eval `dircolors $ZSH_CUSTOM/dircolors_solarized/dircolors.ansi-light`
 
-# Load additional files.
-source $ZSH_CUSTOM/aliases.zsh
+# Load aliases.
+[[ -f ~/.aliases  ]] && source $ZSH_CUSTOM/aliases.zsh
 
 # Use vim as the visual editor
 export VISUAL=vim
@@ -10,3 +10,7 @@ export EDITOR=$VISUAL
 
 # Shiftit
 export SHIFTIT="/home/rap/repos/shiftit"
+
+# Awesome cd movements from zshkit.
+setopt autocd autopushd pushdminus pushdsilent pushdtohome cdablevars
+DIRSTACKSIZE=5
