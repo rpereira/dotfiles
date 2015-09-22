d1da8eb745a721babe5777685fa4727f92aaeacd
Rui Pereira <ruiafonsopereira@gmail.com>
2015-09-22 22:43:35 +0100

    Update path of dircolors.ansi-light

--- zsh/rap.zsh
+++ zsh/rap.zsh
@@ -1,8 +1,16 @@
 # Activate the theme for all future shell sessions.
-eval `dircolors $ZSH_CUSTOM/dircolors_solarized/dircolors.ansi-light`
+# For now, lets just use a hack in order to get this working: all repos related
+# to solarized should sit under ~/.solarized. For some reason, if we specify the
+# path of the expression to eval to
+# ~/solarized/dircolors-solarized/dircolors.ansi-light, this expression returns
+# an error saying that there is no such file on the specified path. Thus, lets
+# just create a symbolic link from
+# ~/solarized/dircolors-solarized/dircolors.ansi-light to
+# ~/repos/dotfiles/zsh/dircolors.ansi-light.
+eval `dircolors $ZSH_CUSTOM/.dircolors.ansi-light`
 
 # Load aliases.
-[[ -f ~/.aliases  ]] && source $ZSH_CUSTOM/aliases.zsh
+[[ -f ~/.aliases ]] && source $ZSH_CUSTOM/aliases.zsh
 
 # Use vim as the visual editor
 export VISUAL=vim
