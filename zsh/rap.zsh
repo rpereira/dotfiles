1b9a64d0bf632368cdee3f592578b70960a375d8
Rui Pereira <ruiafonsopereira@gmail.com>
2015-09-23 10:31:22 +0100

    Load local config

--- zsh/rap.zsh
+++ zsh/rap.zsh
@@ -9,16 +9,15 @@
 # ~/repos/dotfiles/zsh/dircolors.ansi-light.
 eval `dircolors $ZSH_CUSTOM/.dircolors.ansi-light`
 
+# Local dotfiles
+LOCAL_DOTFILES=~/.dotfiles.local
+
 # Load aliases.
 [[ -f ~/.aliases ]] && source $ZSH_CUSTOM/aliases.zsh
-
-# Use vim as the visual editor
-export VISUAL=vim
-export EDITOR=$VISUAL
-
-# Shiftit
-export SHIFTIT="/home/rap/repos/shiftit"
 
 # Awesome cd movements from zshkit.
 setopt autocd autopushd pushdminus pushdsilent pushdtohome cdablevars
 DIRSTACKSIZE=5
+
+# Local config
+[[ -f $LOCAL_DOTFILES/zshrc.local ]] && source ~/$LOCAL_DOTFILES/zshrc.local
