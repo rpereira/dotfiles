a1ad313615ecc5ef6951002664c897f47890ec50
Rui Pereira <ruiafonsopereira@gmail.com>
2015-09-23 14:24:17 +0100

    Install Oh My Zsh

--- install.sh
+++ install.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Install Oh My Zsh
+if ! command -v curl >/dev/null 2>&1; then
+  sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+else if ! command -v wget >/dev/null 2>&1; then
+  sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 fi
 
 
