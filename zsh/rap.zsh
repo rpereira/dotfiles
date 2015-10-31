43abbb16a13bfe7e00ba61f852af8253b44704ef
Rui Afonso Pereira <rpereira@users.noreply.github.com>
2015-10-31 12:02:36 +0000

    Fix issue with GNOME dircolors

--- zsh/rap.zsh
+++ zsh/rap.zsh
@@ -1,13 +1,4 @@
-# Activate the theme for all future shell sessions.
-# For now, lets just use a hack in order to get this working: all repos related
-# to solarized should sit under ~/.solarized. For some reason, if we specify the
-# path of the expression to eval to
-# ~/solarized/dircolors-solarized/dircolors.ansi-light, this expression returns
-# an error saying that there is no such file on the specified path. Thus, lets
-# just create a symbolic link from
-# ~/solarized/dircolors-solarized/dircolors.ansi-light to
-# ~/repos/dotfiles/zsh/dircolors.ansi-light.
-eval `dircolors $ZSH_CUSTOM/.dircolors.ansi-light`
+eval `dircolors $HOME/.dircolors`
 
 # Load aliases.
 [[ -f $ZSH_CUSTOM/aliases.zsh ]] && source $ZSH_CUSTOM/aliases.zsh
