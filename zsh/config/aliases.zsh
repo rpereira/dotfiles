e03f5835d69c8b35672dca2dbba537d4e06e8397
Rui Afonso Pereira <rpereira@users.noreply.github.com>
2015-11-01 15:28:06 +0000

    Add aliases to apt package manager

--- zsh/config/aliases.zsh
+++ zsh/config/aliases.zsh
@@ -7,6 +7,21 @@ alias ll='ls -al'
 alias e="$EDITOR"
 alias vi=nvim
 alias vim=nvim
+LSB_DISTRIBUTOR='lsb_release -i -s'
+
+# Ubuntu specific aliases
+# autocomplete-able apt-xxx aliases
+if [[ "$LSB_DISTRIBUTOR" == "Ubuntu" ]]; then
+  alias acs='apt-cache show'
+  alias agi='sudo apt-get install'
+  alias ag='sudo apt-get'
+  alias agu='sudo apt-get update'
+  alias agug='sudo apt-get upgrade'
+  alias aguu='agu && agug'
+  alias agr='sudo apt-get uninstall'
+  alias agp='sudo apt-get purge'
+  alias aga='sudo apt-get autoremove'
+fi
 
 # General
 alias zshconfig="e ~/.zshrc"
