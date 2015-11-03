77e50ba7a3d6d2f8fd1324a731cff2e704704a1e
Rui Pereira <ruiafonsopereira@gmail.com>
2015-11-03 11:56:54 +0000

    Organize aliases into sections

--- zsh/config/aliases.zsh
+++ zsh/config/aliases.zsh
@@ -1,10 +1,13 @@
-# Set correct term for tmux so solarized theme works in vim.
-alias tmux="TERM=screen-256color-bce tmux"
-
-# Unix
+# --------------------------------------------
+# Navigation
+# --------------------------------------------
 alias l='ls'
 alias ll='ls -al'
 alias ln='ln -v'
+
+# --------------------------------------------
+# Sortcuts
+# --------------------------------------------
 alias e="$EDITOR"
 alias v="$VISUAL"
 alias vi=vim
@@ -12,25 +15,59 @@ alias vi=vim
 # Pretty print the path
 alias path='echo $PATH | tr -s ":" "\n"'
 
+# --------------------------------------------
 # Tmux
+# --------------------------------------------
 alias tat='create_if_needed_and_attach'
 
-# General
-alias zshconfig="e ~/.zshrc"
+# Set correct term for tmux so solarized theme works in vim.
+alias tmux="TERM=screen-256color-bce tmux"
 
+# --------------------------------------------
 # Bundler
+# --------------------------------------------
 alias b="bundle"
 
-# Shiftit
-alias start="bundle exec foreman start -f Procfile.dev"
-alias migrate="foreman run rake db:migrate"
-alias tail_log="tail -f log/development.log"
+# --------------------------------------------
+# Rails
+# --------------------------------------------
 
+# Basic commands
+alias r='rails'
+alias rn='rails new'
+alias rs='rails server'
+alias rg='rails generate'
+alias rc='rails console'
+alias rdb='rails dbconsole'
+alias rd='rails destroy'
+
+# Rake
+alias rr='rake routes'
+alias rm='rake db:migrate'
+
+# Other
+alias devlog='tail -f log/development.log'
+
+# --------------------------------------------
 # Vagrant
+# --------------------------------------------
 alias v="vagrant"
 alias vs="vagrant status"
 alias vh="vagrant halt"
 alias vu="vagrant up"
 
-# Include custom aliases
+# --------------------------------------------
+# GNOME
+# --------------------------------------------
+
+# Lock screen
+alias afk='gnome-screensaver-command --lock'
+
+# Hide/show all desktop icons
+alias hidedesktop='gsettings set org.gnome.desktop.background show-desktop-icons false'
+alias showdesktop='gsettings set org.gnome.desktop.background show-desktop-icons true'
+
+# --------------------------------------------
+# Custom
+# --------------------------------------------
 [ -f ~/.aliases.local ] && source ~/.aliases.local
