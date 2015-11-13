c1c75fcb5b68a6f5ca019aed236400fa46a33d0c
Rui Afonso Pereira <rpereira@users.noreply.github.com>
2015-11-13 10:40:33 +0000

    Implement sudo-keepalive
    Let's just ask for administrator password upfront, and then keep
    alive the existing sudo time stamp ultil the script has finished
    execution.

--- ubuntu.sh
+++ ubuntu.sh
@@ -4,25 +4,37 @@
 # TESTED WITH UBUNTU 14.04 LTS #
 ################################
 
-sudo apt-get update
-sudo apt-get install git
+# Ask for the administrator password upfront.
+sudo -v
 
-sudo apt-get remove vim-tiny
-sudo apt-get install vim
+# Keep-alive: update existing `sudo` time stamp until the script has finished.
+while true; do -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
-sudo apt-get install silversearcher-ag
+# Get information on the newest versions of packages and their dependencies.
+apt-get update
 
-sudo apt-get install xclip
+# Install useful binaries
+apt-get install git
+apt-get install silversearcher-ag
+apt-get install tree
+apt-get install xclip
 
-sudo apt-get install tmux
+# Install more recent version of Vim
+apt-get remove vim-tiny
+apt-get install vim
 
-sudo apt-get install zsh
+# Install THE terminal multiplexer
+apt-get install tmux
+
+# Install the Z shell and oh-my-zsh framework
+apt-get install zsh
 chsh -s $(which zsh)
 sh -c "$(curl -fsSL \
   https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 echo "\n*** Type 'exit' to continue installation!"
 
+# Install zsh syntax highlighting (as you type)
 git clone git://github.com/zsh-users/zsh-syntax-highlighting.git \
   ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 
@@ -42,9 +54,8 @@ git clone https://github.com/seebi/dircolors-solarized \
 git clone https://github.com/seebi/tmux-colors-solarized \
   $SOLARIZED/tmux-colors-solarized
 
-# Clone this repo
+# Clone this repo and change to its directory
 git clone https://github.com/ruiafonsopereira/dotfiles $HOME/repos/dotfiles
-
 cd $HOME/repos/dotfiles || exit
 
 # symlink it up!
