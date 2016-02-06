273eb3705e98596f728fe0d8fc8d4e5061ea4bb9
Rui Pereira <ruipereira@Shiftits-Air.lan>
2016-02-06 11:25:08 +0000

    Add osx stuff
    This is just a quick and naive approach to get things running quick as
    possible on OSX. The installation files regarding OSX and Ubuntu will
    soon been revisited.

---
+++ system/osx/brew.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# Install command-line tools using Homebrew.
+
+# Ask for the administrator password upfront.
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until the script has finished.
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+# Make sure we’re using the latest Homebrew.
+brew update
+
+# Upgrade any already-installed formulae.
+brew upgrade --all
+
+brew install vim --override-system-vi
+brew install tmux
+brew install the_silver_searcher
+
+# Remove outdated versions from the cellar.
+brew cleanup
