632a9c348c94250c75cf5e062f3126b87f348268
Rui Afonso Pereira <rpereira@users.noreply.github.com>
2016-02-06 11:25:08 +0000

    Add osx stuff
    This is just a quick and naive approach to get things running quick as
    possible on OSX. The installation files regarding OSX and Ubuntu will
    soon been revisited.

---
+++ system/osx/symlink_setup.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+DOTFILES=$HOME/.dotfiles
+
+# Symlink dotfiles to our home.
+ln -sf $HOME/code/dotfiles $DOTFILES/.dotfiles
+
+# zsh
+ln -sf $DOTFILES/zsh/zshrc $HOME/.zshrc
+ln -sf $DOTFILES/zsh/zshenv $HOME/.zshenv
+
+# vim
+ln -sf $DOTFILES/vim/vimrc $HOME/.vimrc
+ln -sf $DOTFILES/vim/ftplugin $HOME/.vim/ftplugin
+
+# tmux
+ln -sf $DOTFILES/tmux/tmux.conf $HOME/.tmux.conf
+
+# git
+ln -sf $DOTFILES/git/gitconfig $HOME/.gitconfig
+
+# git diff-highlight
+# See https://github.com/git/git/tree/master/contrib/diff-highlight
+sudo ln -sf /usr/share/doc/git/contrib/diff-highlight/diff-highlight /bin/
+sudo chmod +x /bin/diff-highlight
+
+# TheSilverSearcher ignore list
+ln -sf $DOTFILES/ag/agignore $HOME/.agignore
