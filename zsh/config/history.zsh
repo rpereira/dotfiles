bc6246667814b0b211a49d6c11eab5c6c6dc5920
Rui Pereira <ruiafonsopereira@gmail.com>
2016-02-11 10:27:17 +0000

    Add command history configuration

--- zsh/config/history.zsh
+++ zsh/config/history.zsh
@@ -1,5 +1,19 @@
-setopt hist_ignore_all_dups inc_append_history
-
 HISTFILE=$HOME/.zsh_history
+
 HISTSIZE=10000
 SAVEHIST=10000
+
+# Expire duplicate entries first when trimming history.
+setopt HIST_EXPIRE_DUPS_FIRST
+
+# Delete old recorded entry if new entry is a duplicate.
+setopt HIST_IGNORE_ALL_DUPS
+
+# Remove superfluous blanks before adding to the history list.
+setopt HIST_REDUCE_BLANKS
+
+# Don't write duplicate entries in the history file.
+setopt HIST_SAVE_NO_DUPS
+
+# New history lines are added as soon as they are entered.
+setopt INC_APPEND_HISTORY
