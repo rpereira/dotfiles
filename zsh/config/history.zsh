5df2c99c00528596c4b6ea00ff007baf367f9f44
Rui Pereira <ruiafonsopereira@gmail.com>
2016-03-01 16:37:45 +0000

    Update history settings

--- zsh/config/history.zsh
+++ zsh/config/history.zsh
@@ -3,6 +3,9 @@ HISTFILE=$ZSH/.zsh_history
 HISTSIZE=10000
 SAVEHIST=10000
 
+# Save beginning and ending timestamps to the history file.
+setopt EXTENDED_HISTORY
+
 # Expire duplicate entries first when trimming history.
 setopt HIST_EXPIRE_DUPS_FIRST
 
@@ -15,5 +18,11 @@ setopt HIST_REDUCE_BLANKS
 # Don't write duplicate entries in the history file.
 setopt HIST_SAVE_NO_DUPS
 
+# Do not execute immediately upon history expansion.
+setopt HIST_VERIFY
+
 # New history lines are added as soon as they are entered.
 setopt INC_APPEND_HISTORY
+
+# Share history between all sessions.
+setopt SHARE_HISTORY
