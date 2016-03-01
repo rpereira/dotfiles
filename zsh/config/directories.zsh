9924e2dc7ce6f2020dc10b3460cd9f30b61228f3
Rui Pereira <ruiafonsopereira@gmail.com>
2016-03-01 12:43:02 +0000

    Fix ls colors

--- zsh/config/directories.zsh
+++ zsh/config/directories.zsh
@@ -32,22 +32,5 @@ alias la='ls -lAh'
 alias ll='ls -lh'
 alias lsa='ls -lah'
 
-# Set Solarized dircolors
-if [[ $(uname) == 'FreeBSD' || $(uname) == 'Darwin'  ]]; then
-  LSCOLORS=''
-  LSCOLORS+='gx' # Directory
-  LSCOLORS+='fx' # Symbolic link
-  LSCOLORS+='cx' # Socket
-  LSCOLORS+='dx' # Pipe
-  LSCOLORS+='cx' # Executable
-  LSCOLORS+='eg' # Block special
-  LSCOLORS+='ed' # Character special
-  LSCOLORS+='ab' # Executable with setuid bit set
-  LSCOLORS+='ag' # Executable with setgid bit set
-  LSCOLORS+='cc' # Directory writable to others, with sticky bit
-  LSCOLORS+='bd' # Directory writable to others, without sticky bit
-  export LSCOLORS
-else
-  # TODO
-  #eval `dircolors $HOME/.dircolors`
-fi
+# Set Solarized Dark ls colors
+export LS_COLORS='no=00:fi=00:di=01;34:ln=01;36:pi=40;33:so=01;35:do=01;35:bd=40;33;01:cd=40;33;01:or=40;31;01:ex=01;32:*.tar=01;31:*.tgz=01;31:*.arj=01;31:*.taz=01;31:*.lzh=01;31:*.zip=01;31:*.z=01;31:*.Z=01;31:*.gz=01;31:*.bz2=01;31:*.deb=01;31:*.rpm=01;31:*.jar=01;31:*.jpg=01;35:*.jpeg=01;35:*.gif=01;35:*.bmp=01;35:*.pbm=01;35:*.pgm=01;35:*.ppm=01;35:*.tga=01;35:*.xbm=01;35:*.xpm=01;35:*.tif=01;35:*.tiff=01;35:*.png=01;35:*.mov=01;35:*.mpg=01;35:*.mpeg=01;35:*.avi=01;35:*.fli=01;35:*.gl=01;35:*.dl=01;35:*.xcf=01;35:*.xwd=01;35:*.ogg=01;35:*.mp3=01;35:*.wav=01;35:'
