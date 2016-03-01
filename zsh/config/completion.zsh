31047832cd15383d595019cfc377287b7915958d
Rui Afonso Pereira <rpereira@users.noreply.github.com>
2016-03-01 16:25:14 +0000

    Configure competition

--- zsh/config/completion.zsh
+++ zsh/config/completion.zsh
@@ -1,5 +1,33 @@
 autoload -Uz compinit && compinit
 
-## case-insensitive (all), partial-word and then substring completion
+unsetopt FLOWCONTROL
+unsetopt MENU_COMPLETE
+
+setopt ALWAYS_TO_END
+setopt AUTO_MENU
+setopt COMPLETE_IN_WORD
+setopt EXTENDEDGLOB
+
+WORDCHARS=''
+
+zmodload -i zsh/complist
+
+# case-insensitive (all), partial-word and then substring completion
 zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}' \
        'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
+
+# Autocompletion with an arrow-key driven interface
+zstyle ':completion:*:*:*:*:*' menu select
+
+# disable named-directories autocompletion
+zstyle ':completion:*:cd:*' tag-order local-directories directory-stack path-directories
+
+zstyle ':completion:*:paths' accept-exact '*(N)'
+zstyle ':completion::complete:*' use-cache 1
+zstyle ':completion::complete:*' cache-path $ZSH_CACHE_DIR
+
+# Don't complete uninteresting users
+zstyle ':completion:*:*:*:users' ignored-patterns \
+  man postgres shutdown '_*'
+
+zstyle '*' single-ignored complete
