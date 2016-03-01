8bc3f49eaa7f1ec8bd519ddead3d89b6d7ba0b80
Rui Pereira <ruiafonsopereira@gmail.com>
2016-03-01 11:58:53 +0000

    Enable completion

--- zsh/config/completion.zsh
+++ zsh/config/completion.zsh
@@ -1,3 +1,5 @@
+autoload -Uz compinit && compinit
+
 ## case-insensitive (all), partial-word and then substring completion
 zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}' \
        'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
