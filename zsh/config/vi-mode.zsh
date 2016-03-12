6855fe0242f916e433d85d7a5c8fd06c9e71e021
Rui Afonso Pereira <rpereira@users.noreply.github.com>
2016-03-12 17:11:24 +0000

    Improve zsh vi-mode
    The changes introduced by this commit render the vi-mode status when in
    `normal` mode in the right prompt.
    
    Also, since the majority of key-bindings that were defined in the
    key-bindings.zsh file were related to vi-mode, let's extract then to the
    new file `vi-mode.zsh`.

---
+++ zsh/config/vi-mode.zsh
@@ -0,0 +1,35 @@
+# Updates editor information when the keymap changes.
+function zle-keymap-select() {
+  zle reset-prompt
+  zle -R
+}
+
+# Ensure that the prompt is redrawn when the terminal size changes.
+TRAPWINCH() {
+  zle && { zle reset-prompt; zle -R }
+}
+
+zle -N zle-keymap-select
+zle -N edit-command-line
+
+bindkey -v
+
+# allow v to edit the command line (standard behaviour)
+autoload -Uz edit-command-line
+bindkey -M vicmd 'v' edit-command-line
+
+# allow ctrl-p, ctrl-n for navigate history (standard behaviour)
+bindkey '^P' up-history
+bindkey '^N' down-history
+
+# allow ctrl-h, ctrl-w, ctrl-? for char and word deletion (standard behaviour)
+bindkey '^?' backward-delete-char
+bindkey '^h' backward-delete-char
+bindkey '^w' backward-kill-word
+
+# Let's just show the NORMAL mode indicator
+MODE_INDICATOR="%{$fg_bold[red]%}NORMAL%{$reset_color%}"
+
+function vi_mode_prompt_info() {
+  echo "${${KEYMAP/vicmd/$MODE_INDICATOR}/(main|viins)/}"
+}
