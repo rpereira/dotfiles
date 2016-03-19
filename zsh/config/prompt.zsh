7305f2c8426835f973c99121d7cd07d60717ef0e
Rui Afonso Pereira <rpereira@users.noreply.github.com>
2016-03-19 11:34:53 +0000

    Move git prompt function to proper file
    This commit also updates the prompt char.

--- zsh/config/prompt.zsh
+++ zsh/config/prompt.zsh
@@ -28,45 +28,16 @@ ZSH_THEME_GIT_PROMPT_UNTRACKED="%{$fg[grey]%}◒ "
 
 # Set the prompt's char color to red when the command line return an error;
 # default is white.
-local prompt_char="%(?:%{$fg_bold[white]%}$ :%{$fg_bold[red]%}$ %s)"
+local prompt_char=$(echo "\u2794 ")
+local return_status="%(?:%{$fg_bold[white]%}$prompt_char:%{$fg_bold[red]%}$prompt_char%s)"
 
 # Display the whole current directory path.
 local current_dir="%{$fg[blue]%}%~%{$reset_color%} %s"
-
-# Determine the time elapsed since last commit.
-function _git_time_since_commit() {
-  # Only proceed if there is actually a commit.
-  if git log -1 > /dev/null 2>&1; then
-    # Get the last commit.
-    last_commit=$(git log --pretty=format:'%at' -1 2> /dev/null)
-    now=$(date +%s)
-    seconds_since_last_commit=$((now-last_commit))
-
-    # Totals
-    minutes=$((seconds_since_last_commit / 60))
-    hours=$((seconds_since_last_commit / 3600))
-
-    # Sub-hours and sub-minutes
-    days=$((seconds_since_last_commit / 86400))
-    sub_hours=$((hours % 24))
-    sub_minutes=$((minutes % 60))
-
-    if [ $hours -gt 24 ]; then
-      commit_age="${days}d"
-    elif [ $minutes -gt 60 ]; then
-      commit_age="${sub_hours}h${sub_minutes}m"
-    else
-      commit_age="${minutes}m"
-    fi
-
-    echo "%{$reset_color%}$commit_age"
-  fi
-}
 
 setopt PROMPT_SUBST
 
 PROMPT='
 ${current_dir} $(git_prompt_info)
-${prompt_char}%{$reset_color%}'
+${return_status}%{$reset_color%}'
 
-RPROMPT='%{$(echotc UP 1)%}$(vi_mode_prompt_info) $(_git_time_since_commit) $(git_prompt_status)%{$(echotc DO 1)%}%{$reset_color%}'
+RPROMPT='%{$(echotc UP 1)%}$(vi_mode_prompt_info) $(git_time_since_last_commit) $(git_prompt_status)%{$(echotc DO 1)%}%{$reset_color%}'
