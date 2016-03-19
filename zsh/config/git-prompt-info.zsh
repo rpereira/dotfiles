cf16d192ce98e679fcc6cac25f835868f215060d
Rui Afonso Pereira <ruiafonsopereira@gmail.com>
2016-03-19 11:34:53 +0000

    Move git prompt function to proper file
    This commit also updates the prompt char.

--- zsh/config/git-prompt-info.zsh
+++ zsh/config/git-prompt-info.zsh
@@ -140,3 +140,33 @@ function git_prompt_status() {
   fi
   echo $STATUS
 }
+
+# Determine the time elapsed since last commit.
+function git_time_since_last_commit() {
+  # Only proceed if there is actually a commit.
+  if git log -1 > /dev/null 2>&1; then
+    # Get the last commit.
+    last_commit=$(git log --pretty=format:'%at' -1 2> /dev/null)
+    now=$(date +%s)
+    seconds_since_last_commit=$((now-last_commit))
+
+    # Totals
+    minutes=$((seconds_since_last_commit / 60))
+    hours=$((seconds_since_last_commit / 3600))
+
+    # Sub-hours and sub-minutes
+    days=$((seconds_since_last_commit / 86400))
+    sub_hours=$((hours % 24))
+    sub_minutes=$((minutes % 60))
+
+    if [ $hours -gt 24 ]; then
+      commit_age="${days}d"
+    elif [ $minutes -gt 60 ]; then
+      commit_age="${sub_hours}h${sub_minutes}m"
+    else
+      commit_age="${minutes}m"
+    fi
+
+    echo "%{$reset_color%}$commit_age"
+  fi
+}
