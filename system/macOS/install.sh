a5e448e503118eee62f03d55dd8d239a3f18b965
Rui Afonso Pereira <rpereira@users.noreply.github.com>
2016-08-20 19:51:10 +0100

    Rename file from macOS.sh to install.sh

--- system/macOS/install.sh
+++ system/macOS/install.sh
@@ -1,11 +1,16 @@
 #!/usr/bin/env bash
 
-fancy_echo() {
+info() {
   local fmt="$1"; shift
 
   # shellcheck disable=SC2059
   printf "\n$fmt\n" "$@"
 }
+
+export DOTFILES_DIR=$HOME/.dotfiles
+export SOLARIZED_DIR=$HOME/.solarized
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Ask for the administrator password upfront.
 sudo -v
@@ -13,39 +18,52 @@ sudo -v
 # Keep-alive: update existing `sudo` time stamp until the script has finished.
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
-# Install Homebrew and cask
+# Install Xcode Command Line Tools
+if [ -z "$(xcode-select -p)" ]; then
+  xcode-select --install
+fi
+
+# Install Homebrew and Cask
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew tap caskroom/cask
 
 # Install Casks
-./system/macOS/casks.sh
+# shellcheck disable=SC1090
+source "$DIR/casks.sh"
 
 # Install Homebrew formulaes
-./system/macOS/brew.sh
+# shellcheck disable=SC1090
+source "$DIR/brew.sh"
 
 # Create a folder to hold all the solarized color schemes
-mkdir -p ${SOLARIZED:=$HOME/.solarized}
+mkdir -p "$SOLARIZED_DIR"
 
 # Download solarized color schemes
 git clone git@github.com:tomislav/osx-terminal.app-colors-solarized.git \
-  $SOLARIZED/osx-terminal.app-colors-solarized
+  "$SOLARIZED_DIR/osx-terminal.app-colors-solarized"
 
 git clone git@github.com:seebi/dircolors-solarized.git \
-  $SOLARIZED/dircolors-solarized
+  "$SOLARIZED_DIR/dircolors-solarized"
 
-# symlink it up!
-./system/macOS/symlink_setup.sh
+# Symlink it up!
+# shellcheck disable=SC1090
+source "$DIR/symlink_setup.sh"
 
 # Compile manpages
-make -f $HOME/.dotfiles/man/Makefile
+make -f "$DOTFILES_DIR/man/Makefile"
 
 # Compile ZSH-related files for faster autoloading
-zcompile $HOME/.zshrc
-zcompile $HOME/.zshenv
-zcompile $HOME/.zprofile
+declare -a ZFILES=('zshrc' 'zshenv' 'zprofile')
+for zfile in "${ZFILES[@]}"; do
+  zcompile "$HOME/.$zfile"
+done
+unset ZFILES
+
+# Append /usr/local/bin/zsh to the end of /etc/shells file
+grep -q -F '/usr/local/bin/zsh2' /etc/shells || sudo bash -c "echo /usr/local/bin/zsh2 >> /etc/shells"
 
 # Make ZSH the default shell
-chsh -s $(which zsh)
+chsh -s "$(which zsh)"
 
 # Install vim plugin manager
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
@@ -55,4 +73,5 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
 vim +PlugInstall +qall
 
 # Set user preferences
-./system/macOS/defaults.sh
+# shellcheck disable=SC1090
+source "$DIR/defaults.sh"
