76cc908737d250098b7ae218613bd2b7f742c76c
Rui Afonso Pereira <rpereira@users.noreply.github.com>
2016-08-20 17:08:54 +0100

    Improve installation files

---
+++ system/macOS/casks.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+#
+# Install macOS applications using Homebrew Cask.
+
+info "Installing Dropbox"
+brew cask install dropbox
+
+info "Installing Atom"
+brew cask install atom
+
+info "Installing Evernote"
+brew cask install evernote
+
+info "Installing Firefox"
+brew cask install firefox
+
+info "Installing Google Chrome"
+brew cask install google-chrome
+
+info "Installing iTerm2"
+brew cask install iterm2
+
+info "Installing Postgres.app"
+brew cask install postgres
+
+info "Installing Slack"
+brew cask install slack
+
+info "Installing SourceTree"
+brew cask install sourcetree
+
+# Apps not on Cask but I want regardless:
+# * Markoff
--- system/macOS/symlink_setup.sh
+++ system/macOS/symlink_setup.sh
@@ -1,32 +1,30 @@
 #!/usr/bin/env bash
 
-DOTFILES=$HOME/.dotfiles
-
 # Symlink dotfiles to our home.
-ln -sf $HOME/code/dotfiles $DOTFILES
+ln -sf "$HOME/code/dotfiles" "$DOTFILES_DIR"
 
-ln -sf $HOME/.solarized/dircolors-solarized/dircolors.256dark $HOME/.dircolors
+ln -sf "$SOLARIZED_DIR/dircolors-solarized/dircolors.256dark" "$HOME/.dircolors"
 
 # zsh
-ln -sf $DOTFILES/zsh/zshrc $HOME/.zshrc
-ln -sf $DOTFILES/zsh/zshenv $HOME/.zshenv
-ln -sf $DOTFILES/zsh/zprofile $HOME/.zprofile
+ln -sf "$DOTFILES_DIR/zsh/zshrc" "$HOME/.zshrc"
+ln -sf "$DOTFILES_DIR/zsh/zshenv" "$HOME/.zshenv"
+ln -sf "$DOTFILES_DIR/zsh/zprofile" "$HOME/.zprofile"
 
 # vim
-ln -sf $DOTFILES/vim/vimrc $HOME/.vimrc
-ln -sf $DOTFILES/vim/gvimrc $HOME/.gvimrc
-ln -sf $DOTFILES/vim/ftplugin $HOME/.vim/after
-ln -sf $DOTFILES/vim/UltiSnips $HOME/.vim/UltiSnips
+ln -sf "$DOTFILES_DIR/vim/vimrc" "$HOME/.vimrc"
+ln -sf "$DOTFILES_DIR/vim/gvimrc" "$HOME/.gvimrc"
+ln -sf "$DOTFILES_DIR/vim/ftplugin" "$HOME/.vim/after"
+ln -sf "$DOTFILES_DIR/vim/UltiSnips" "$HOME/.vim/UltiSnips"
 
 # tmux
-ln -sf $DOTFILES/tmux/tmux.conf $HOME/.tmux.conf
+ln -sf "$DOTFILES_DIR/tmux/tmux.conf" "$HOME/.tmux.conf"
 
 # git
-ln -sf $DOTFILES/git/gitconfig $HOME/.gitconfig
-ln -sf $DOTFILES/git/gitignore_global $HOME/.gitignore_global
+ln -sf "$DOTFILES_DIR/git/gitconfig" "$HOME/.gitconfig"
+ln -sf "$DOTFILES_DIR/git/gitignore_global" "$HOME/.gitignore_global"
 
-# TheSilverSearcher ignore list
-ln -sf $DOTFILES/ag/agignore $HOME/.agignore
+# TheSi"lverSearcher ignore list
+ln -sf "$DOTFILES_DIR/ag/agignore" "$HOME/.agignore"
 
-# ctags
-ln -sf $DOTFILES/ctags/ctags $HOME/.ctags
+# ctags"
+ln -sf "$DOTFILES_DIR/ctags/ctags" "$HOME/.ctags"
