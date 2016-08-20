40277e2dc78ab7dd87769708a62375b02acd2b6e
Rui Afonso Pereira <ruiafonsopereira@gmail.com>
2016-08-20 17:08:54 +0100

    Improve installation files

--- system/macOS/brew.sh
+++ system/macOS/brew.sh
@@ -1,19 +1,6 @@
 #!/usr/bin/env bash
-
+#
 # Install command-line tools using Homebrew.
-
-# Ask for the administrator password upfront.
-sudo -v
-
-# Keep-alive: update existing `sudo` time stamp until the script has finished.
-while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
-
-fancy_echo() {
-  local fmt="$1"; shift
-
-  # shellcheck disable=SC2059
-  printf "\n$fmt\n" "$@"
-}
 
 # Make sure we’re using the latest Homebrew.
 brew update
@@ -21,7 +8,7 @@ brew update
 # Upgrade any already-installed formulae.
 brew upgrade --all
 
-fancy_echo "Installing GNU tools..."
+info "Installing GNU tools..."
 # Install GNU core utilities (those that come with macOS are outdated).
 brew install coreutils
 # Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
@@ -31,7 +18,7 @@ brew install gnu-sed --with-default-names
 # Install `wget` with IRI support.
 brew install wget --with-iri
 
-fancy_echo "Installing Unix tools..."
+info "Installing Unix tools..."
 brew install git
 brew install ctags
 brew install diff-so-fancy
@@ -48,24 +35,20 @@ brew install zsh-completions
 brew install zsh-history-substring-search
 brew install zsh-syntax-highlighting
 
-fancy_echo "Installing programming languages..."
+info "Installing programming languages..."
 brew install elixir
-brew install node
 brew install nodenv
 brew install rbenv
 brew install ruby-build
 brew install rust
 
-fancy_echo "Installing databases..."
-brew install postgres
-
-fancy_echo "Installing other useful binaries..."
+info "Installing other useful binaries..."
 brew install dos2unix
 brew install imagemagick --with-webp
 brew install pandoc
 brew install shellcheck
 
-fancy_echo "Installing OpenPG tools..."
+info "Installing OpenPG tools..."
 brew install gnupg2 gpg-agent pinentry-mac
 
 # Remove outdated versions from the cellar.
--- system/macOS/install.sh
+++ system/macOS/install.sh
@@ -1,16 +1,11 @@
 #!/usr/bin/env bash
 
-info() {
+fancy_echo() {
   local fmt="$1"; shift
 
   # shellcheck disable=SC2059
   printf "\n$fmt\n" "$@"
 }
-
-export DOTFILES_DIR=$HOME/.dotfiles
-export SOLARIZED_DIR=$HOME/.solarized
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Ask for the administrator password upfront.
 sudo -v
@@ -18,52 +13,39 @@ sudo -v
 # Keep-alive: update existing `sudo` time stamp until the script has finished.
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
-# Install Xcode Command Line Tools
-if [ -z "$(xcode-select -p)" ]; then
-  xcode-select --install
-fi
-
-# Install Homebrew and Cask
+# Install Homebrew and cask
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew tap caskroom/cask
 
 # Install Casks
-# shellcheck disable=SC1090
-source "$DIR/casks.sh"
+./system/macOS/casks.sh
 
 # Install Homebrew formulaes
-# shellcheck disable=SC1090
-source "$DIR/brew.sh"
+./system/macOS/brew.sh
 
 # Create a folder to hold all the solarized color schemes
-mkdir -p "$SOLARIZED_DIR"
+mkdir -p ${SOLARIZED:=$HOME/.solarized}
 
 # Download solarized color schemes
 git clone git@github.com:tomislav/osx-terminal.app-colors-solarized.git \
-  "$SOLARIZED_DIR/osx-terminal.app-colors-solarized"
+  $SOLARIZED/osx-terminal.app-colors-solarized
 
 git clone git@github.com:seebi/dircolors-solarized.git \
-  "$SOLARIZED_DIR/dircolors-solarized"
+  $SOLARIZED/dircolors-solarized
 
-# Symlink it up!
-# shellcheck disable=SC1090
-source "$DIR/symlink_setup.sh"
+# symlink it up!
+./system/macOS/symlink_setup.sh
 
 # Compile manpages
-make -f "$DOTFILES_DIR/man/Makefile"
+make -f $HOME/.dotfiles/man/Makefile
 
 # Compile ZSH-related files for faster autoloading
-declare -a ZFILES=('zshrc' 'zshenv' 'zprofile')
-for zfile in "${ZFILES[@]}"; do
-  zcompile "$HOME/.$zfile"
-done
-unset ZFILES
-
-# Append /usr/local/bin/zsh to the end of /etc/shells file
-grep -q -F '/usr/local/bin/zsh2' /etc/shells || sudo bash -c "echo /usr/local/bin/zsh2 >> /etc/shells"
+zcompile $HOME/.zshrc
+zcompile $HOME/.zshenv
+zcompile $HOME/.zprofile
 
 # Make ZSH the default shell
-chsh -s "$(which zsh)"
+chsh -s $(which zsh)
 
 # Install vim plugin manager
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
@@ -73,5 +55,4 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
 vim +PlugInstall +qall
 
 # Set user preferences
-# shellcheck disable=SC1090
-source "$DIR/defaults.sh"
+./system/macOS/defaults.sh
