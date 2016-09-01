808039287424ee7faefb962b8ece76d560784866
Rui Afonso Pereira <ruiafonsopereira@gmail.com>
2016-09-01 07:58:05 +0100

    Add Mac Apple Store defaults
    New defaults:
      * Enable the automatic update check
      * Download newly available updates in background
      * Install System data files & security updates
      * Automatically download apps purchased on other Macs

--- system/macOS/defaults.sh
+++ system/macOS/defaults.sh
@@ -17,6 +17,21 @@ defaults write com.apple.ActivityMonitor SortColumn -string "CPUUsage"
 defaults write com.apple.ActivityMonitor SortDirection -int 0
 
 # ==============================================
+# App Store
+# ==============================================
+# Enable the automatic update check
+defaults write com.apple.SoftwareUpdate AutomaticCheckEnabled -bool true
+
+# Download newly available updates in background
+defaults write com.apple.SoftwareUpdate AutomaticDownload -int 1
+
+# Install System data files & security updates
+defaults write com.apple.SoftwareUpdate CriticalUpdateInstall -int 1
+
+# Automatically download apps purchased on other Macs
+defaults write com.apple.SoftwareUpdate ConfigDataInstall -int 1
+
+# ==============================================
 # Calendar (iCal)
 # ==============================================
 # Days per week: 7
