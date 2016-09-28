4e3abd4affd6316309261bd6f153d02503df07e6
Rui Afonso Pereira <ruiafonsopereira@gmail.com>
2016-09-28 09:41:35 +0100

    Extract aliases to specific modules

---
+++ zsh/modules/ruby.zsh
@@ -0,0 +1,17 @@
+# Return if ruby and rbenv are not found.
+if ! command -v ruby >/dev/null; then
+  echo "Error loading $0: Module requires 'ruby' to be installed."
+  return 1
+fi
+
+if ! command -v rbenv >/dev/null; then
+  echo "Error loading $0: Module requires 'rbenv' to be installed."
+  return 1
+fi
+
+#
+# Aliases
+#
+
+# Bundler
+alias b="bundle"
