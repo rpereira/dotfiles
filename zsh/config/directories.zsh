38c03783b885a2173dedbace0df7d7c5a3db04a8
Rui Afonso Pereira <ruiafonsopereira@gmail.com>
2016-09-28 08:57:25 +0100

    :memo: Document options used to customize the directory stack

--- zsh/config/directories.zsh
+++ zsh/config/directories.zsh
@@ -1,11 +1,19 @@
 #------------------------------------------------
 # Change, make and remove directory
 #------------------------------------------------
+# Auto changes to a directory without typing cd.
 setopt AUTO_CD
-setopt AUTO_PUSHD
-setopt PUSHDMINUS
-setopt PUSHD_IGNORE_DUPS
 
+# Push the old directory onto the stack on cd.
+setopt AUTO_PUSHD
+
+# Swap the meaning of cd +1 and cd -1; we want them to mean the opposite of
+# what they mean in csh, because it makes more sense in this scheme, and it's
+# easier to type.
+setopt PUSHDMINUS
+
+# Do not store duplicates in the stack.
+setopt PUSHD_IGNORE_DUPS
 
 alias -g ...='../..'
 alias -g ....='../../..'
