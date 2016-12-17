00feec05ed07457473fa9d8daaf53847768c3d96
Rui Afonso Pereira <rpereira@users.noreply.github.com>
2016-12-17 09:49:17 +0000

    Move fuzzy cd to shell function

--- zsh/config/functions.zsh
+++ zsh/config/functions.zsh
@@ -1,26 +1,10 @@
-function extract {
-  echo Extracting $1 ...
-  if [ -f $1 ] ; then
-      case $1 in
-          *.tar.bz2)   tar xjf $1  ;;
-          *.tar.gz)    tar xzf $1  ;;
-          *.bz2)       bunzip2 $1  ;;
-          *.rar)       unrar x $1    ;;
-          *.gz)        gunzip $1   ;;
-          *.tar)       tar xf $1   ;;
-          *.tbz2)      tar xjf $1  ;;
-          *.tgz)       tar xzf $1  ;;
-          *.zip)       unzip $1   ;;
-          *.Z)         uncompress $1  ;;
-          *.7z)        7z x $1  ;;
-          *)        echo "'$1' cannot be extracted via extract()" ;;
-      esac
-  else
-      echo "'$1' is not a valid file"
-  fi
+# Create a new directory and enter it.
+mkcd() {
+  mkdir -p "./$1" && cd "./$1"
 }
 
-# Create a new directory and enter it.
-mkcd () {
-  mkdir -p "./$1" && cd "./$1"
+# Fuzzy cd into selected directory.
+fcd() {
+  DIR=`find ${1:-*} -path '*/\.*' -prune -o -type d -print 2> /dev/null | fzf-tmux` \
+    && cd "$DIR"
 }
