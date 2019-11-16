017f600729a75aab6d82c3acc75941b799ec2f4e
Rui Afonso Pereira <ruiafonsopereira@gmail.com>
2019-11-16 08:36:51 +0000

    Add command to browse Google Chrome history from terminal

--- zsh/config/functions.zsh
+++ zsh/config/functions.zsh
@@ -15,3 +15,25 @@ docker_clean() {
   docker rm -vf $(docker ps -a -q)
   docker rmi -f $(docker images -a -q)
 }
+
+# Browse Google Chrome history and open selected link in default browser.
+c() {
+  local cols sep
+  export cols=$(( COLUMNS / 3 ))
+  export sep='{::}'
+
+  cp -f ~/Library/Application\ Support/Google/Chrome/Default/History /tmp/h
+  sqlite3 -separator $sep /tmp/h \
+    "select title, url from urls order by last_visit_time desc" |
+  ruby -ne '
+    cols = ENV["cols"].to_i
+    title, url = $_.split(ENV["sep"])
+    len = 0
+    puts "\x1b[36m" + title.each_char.take_while { |e|
+      if len < cols
+        len += e =~ /\p{Han}|\p{Katakana}|\p{Hiragana}|\p{Hangul}/ ? 2 : 1
+      end
+    }.join + " " * (2 + cols - len) + "\x1b[m" + url' |
+  fzf --ansi --multi --no-hscroll --tiebreak=index |
+  sed 's#.*\(https*://\)#\1#' | xargs open
+}
